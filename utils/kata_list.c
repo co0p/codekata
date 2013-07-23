0ed5b0c22bc4f75441c98eec514b643af4690db5
jev <eb120@hw.ac.uk>
2013-07-24 02:06:46 +0300

    added list_append()

--- utils/kata_list.c
+++ utils/kata_list.c
@@ -59,25 +59,68 @@ inline long list_length(list_t *list) {
 }
 
 
-long list_insert(list_t *l, void *value, int order) {
-  fprintf(stderr,"ERROR: () not im plemented yet\n");
-  exit(EXIT_FAILURE);
+// returns th eindex of the element in the list
+// order can be 0 -> no order (use append)
+//              1 -> ascending, before a value that is larger    TODO
+//             -1 -> descending, befaore a value that is smaller TODO
+long list_insert(list_t *list, void *value, int order) {
+  if (order == 0) {
+    return list_append(list, value);
+  }
+  else {
+      fprintf(stderr,"ERROR: list_insert(): order != 0, not im plemented yet\n");
+      exit(EXIT_FAILURE);
+  }
 }
 
-long list_append(list_t *l, void *value) {
-  fprintf(stderr,"ERROR: () not im plemented yet\n");
-  exit(EXIT_FAILURE);
+
+// (!) the user is responsible for freeing memory once no longer needed
+// usually returned by remove or pass destroy-elem-fn to list_destroy()
+long list_append(list_t *list, void *value) {
+  long idx = 1;
+  list_elem_t *new_elem, *elem_ptr;
+
+  assert(list != NULL);
+
+  if ((new_elem = malloc(sizeof(list_elem_t))) == NULL) {
+    fprintf(stderr, "FATAL ERROR: list_appen(): out of space\n");
+    exit(EXIT_FAILURE);
+  }
+
+  new_elem->next  = NULL;
+  new_elem->value = value;
+
+  elem_ptr = list->first;
+  while (elem_ptr != NULL) {
+    elem_ptr = elem_ptr->next;
+    ++idx;
+  }
+
+  if (elem_ptr == NULL) {          // append new element
+    list->first = new_elem;
+  }
+  else {
+    elem_ptr->next = new_elem;
+  }
+  list->last  = new_elem;
+  list->length++;
+
+  return idx; // starting with 1 (not 0, as for array indices)
 }
+
 
 long list_prepend(list_t *l, void *value) {
   fprintf(stderr,"ERROR: () not im plemented yet\n");
   exit(EXIT_FAILURE);
 }
 
-list_elem_t *list_remove(list_t *l, void *value) {
+
+list_elem_t *list_remove(list_t *list, void *value) {
   fprintf(stderr,"ERROR: () not im plemented yet\n");
   exit(EXIT_FAILURE);
+  // find value and remove, usess comapre function
 }
+
 
 long list_remove_dupes(list_t *l, void *value) {
   fprintf(stderr,"ERROR: () not im plemented yet\n");
@@ -85,15 +128,18 @@ long list_remove_dupes(list_t *l, void *value) {
 }
  // TODO: need a function as argument to remove values
 
+
 list_elem_t *list_search(void *value) {
   fprintf(stderr,"ERROR: () not im plemented yet\n");
   exit(EXIT_FAILURE);
 }
 
+
 long list_sort(list_t *l, int order) {
   fprintf(stderr,"ERROR: () not im plemented yet\n");
   exit(EXIT_FAILURE);
 }
+
 
 void list_print(list_t *l) {
   fprintf(stderr,"ERROR: () not im plemented yet\n");
--- utils/list_test.c
+++ utils/list_test.c
@@ -29,9 +29,21 @@ int int_cmp(void *a, void *b) {
 
 int main(void) {
   list_t *list;
+  int *x;
+  long ret;
+
+  x = malloc(sizeof(int)); // no error checking done
+  *x = 3;
 
   list = list_create(int_print, int_cmp);
-  printf("list->length = %d\n", list_length(list));
+
+  assert(list_length(list) == 0);
+
+  ret  = list_insert(list, (void *) x, 0);
+  assert(ret == 1);
+  assert(list_length(list)== 1);
+
+  // TODO remove element from the list or provide freeing function
   list_destroy(list, NULL);
 
   return EXIT_SUCCESS;
