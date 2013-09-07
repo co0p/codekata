fd64933fa1565516b5475a7385fdd045e9051b1c
jev <eb120@hw.ac.uk>
2013-09-07 12:39:05 +0300

    added input checks and command line args use for calender

--- 13/cal.c
+++ 13/cal.c
@@ -27,30 +27,44 @@ int get_week_day(int day, int month, int year, int mode) {
 }
 
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-    int year, month, date;
+    int year, month, date, i;
     int startingDay;     /* of the week: init from user input*/
+    int padding[12] = { 2,  1,  4,  4,  6,  5,  5,  3,  0,  2,  1,  1}; /* '-' chars to append to month headling */
+    int months[12]  = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     char *names[] = {"January", "February", "March", "April", "May", "June",
                     "July", "August", "September", "October", "November", "December"};
 
-    printf("Enter the year of your desired calendar: ");
-    scanf("%d", &year);
+    if (argc > 1) {
+        year = atoi(argv[1]);
+    }
+    else {
+        printf("Enter the year of your desired calendar: ");
+        scanf("%d", &year);
+    }
 
-    // could check whether input is valid here
+    if (year <= 0 || year > 65535) {
+        fprintf(stderr,"ERROR: invalid year (should be small positive integer)\n");  // or > 1532 ?
+        exit(EXIT_FAILURE);
+    }
 
     startingDay = get_week_day(1, 1, year,GREGORIAN);
 
-    int months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
     if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-       months[1] = 29;
+       months[1] = 29;  // leap year
 
     for (month = 0; month < 12; ++month) {
         const int daysInMonth = months[month];  /* set # of days */
         int dayOfWeek, date;
 
-        printf("\n  ------------%s-------------\n", names[month]);   // month name
+        printf("\n  ------------%s------------", names[month]);   // month name
+        for (i = 0; i < padding[month]; ++i) {
+            printf("-");
+        }
+        printf("\n");
+
         printf("  Sun  Mon  Tue  Wed  Thu  Fri  Sat\n");         // days of week
 
         for (dayOfWeek = 0; dayOfWeek < startingDay; ++dayOfWeek)
