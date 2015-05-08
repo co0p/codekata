92521acbff4c726e1a9573d92a63469ea66a74bd
jev <eb120@hw.ac.uk>
2015-05-08 10:17:43 +0100

    bugfix in fizzbuzz.hs (guard oder matters) and added a check for missing commnad line argument

--- 02/fizzbuzz.hs
+++ 02/fizzbuzz.hs
@@ -7,17 +7,17 @@
 import System.Environment(getArgs)
 
 fizzbuzz :: Int -> String
-fizzbuzz x | x `mod` 3 == 0 = "fizz"
+fizzbuzz x | (x `mod` 3 == 0) && (x `mod` 5 == 0) = "fizzbuzz"
+           | x `mod` 3 == 0 = "fizz"
            | x `mod` 5 == 0 = "buzz"
-           | (x `mod` 3 == 0) && (x `mod` 5 == 0) = "fizzbuzz"
            | otherwise = show x
            
 main = do 
          args <- getArgs
-          -- TODO: add check:if null args 
-          --   then return error "usage: ./fizzbuzz <n>" 
-          --   else do
-         let
-           n   = read (args!!0) :: Int   -- parse an integer
-           fbs = map fizzbuzz [1..n-1]
-         putStr $ unlines fbs 
+         if null args 
+           then error "usage: ./fizzbuzz <n>" 
+           else do
+                  let
+                    n = read (args!!0) :: Int   -- parse an integer
+                    fbs = map fizzbuzz [1..n-1]
+                  putStr $ unlines fbs 
